38f1605a6973098bc89f10d18e22d1492dfd84b7
Salcuni Giovanni <salcuni@PALEXPO-LT-GS.orgex.po>
2024-07-03 13:43:40 +0200

    Debut de la page user

--- src/app/utilisateur/[idUser]/profile/page.tsx
+++ src/app/utilisateur/[idUser]/profile/page.tsx
@@ -1,9 +1,15 @@
+import { Avatar, Card, CardBody } from '@nextui-org/react'
 import React from 'react'
 
-export default function UtilisateurProfile() {
+export default async function UtilisateurProfile() {
   return (
-    <div>
-        UtilisateurProfile
+    <div className='mx-10'>
+      <Card className='flex flex-col md:flex-row max-w-lg mx-auto mt-10'>
+        <Avatar></Avatar>
+        <CardBody>
+          <h1>Profile</h1>
+        </CardBody>
+      </Card>
     </div>
   )
 }
